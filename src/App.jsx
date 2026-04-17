31b0d9e2eb04622fe07e5065311e3e6108ab4515
Himanshu Pradhan <64770860+himanshupradhann@users.noreply.github.com>
2026-04-17 23:43:30 +0530

    Enhance alert message with multilingual support
    Updated alert message to include multilingual prompts for filling required fields.

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,15 @@ export default function App() {
   }, []);
 
   const generateLink = () => {
-    if (!subject || !year) return alert("Fill all fields");
+    if (!subject || !year) return alert("\n 
+Bhosdi ke 😄 please fill_in all required fields \n
+भोसड़ी के 😄 कृपया सभी आवश्यक फ़ील्ड भरें। \n
+ଭୋସଡି କେ 😄 ଦୟାକରି ସମସ୍ତ ଆବଶ୍ୟକ ଖେତ୍ର ପୂରଣ କରନ୍ତୁ। \n
+Bhosdi ke 😄 veuillez remplir tous les champs obligatoires \n
+Bhosdi ke 😄 por favor, completa todos los campos obligatorios \n
+Bhosdi ke 😄 请填写所有必填字段 \n
+போஸ்டி கே 😄 தயவுசெய்து அனைத்து தேவையான புலங்களையும் நிரப்பவும்\n
+ഭോസ്ഡി കെ 😄 ദയവായി എല്ലാ ആവശ്യമായ ഫീൽഡുകളും പൂരിപ്പിക്കുക \n");
 
     let yearPart = "";
 
